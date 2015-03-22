ebe91e84a0767a84f039932d756c76de8012eebb
AmeliaL <namel.lopes@gmail.com>
2015-03-22 16:27:40 +0100

    essai 1
    transfert de dossier

---
+++ Assets/scripts/trigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+public class trigger : MonoBehaviour {
+	public Transform light;
+	private Light pikachu;
+	// Use this for initialization
+	void Start () {
+		//renderer.enabled (false);
+		pikachu = light.GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+
+		
+
+	void OnTriggerEnter(Collider col)
+	{
+
+			pikachu.enabled = false;
+
+			
+
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+				pikachu.enabled = true;
+		}
+}
---
+++ Assets/scripts/trigger1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+public class trigger1 : MonoBehaviour {
+	public Transform light;
+	private Light pika;
+		// Use this for initialization
+	void Start () {
+		//renderer.enabled (false);
+		pika = light.GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	
+	
+	
+	void OnTriggerEnter(Collider col)
+	{
+		
+		pika.enabled = false;
+		
+		
+		
+	}
+	
+	void OnTriggerExit(Collider col)
+	{
+		pika.enabled = true;
+	}
+}
